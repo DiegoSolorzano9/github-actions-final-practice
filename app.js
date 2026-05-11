8eb7bd83eaf29c755a5108fc77c387df3582ba97
Diego Solorzano <solorzano.arancibia.diegosantiago@usfx.bo>
2026-05-10 22:01:53 -0400

    fix: corrige error de linting

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-const unusedVariable = 'error intencional';
 
 // Entidad de dominio: Tareas (Todos)
 let todos = [
